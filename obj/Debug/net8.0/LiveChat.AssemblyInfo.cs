e1d1b40998283d5dbb39dc3b466b42522007e80d
Jasmin Džubur <dzuburjasmin13@gmail.com>
2024-08-06 13:31:11 +0200

    supp
    disable https

--- obj/Debug/net8.0/LiveChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/LiveChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe6a674a43d7813e6aaa01b7d59ae2e8fcbfc84")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
