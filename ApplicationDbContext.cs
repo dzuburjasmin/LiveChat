8568662abfed9fca62a289ca6f7e09227c969cda
Jasmin Džubur <dzuburjasmin13@gmail.com>
2024-08-19 19:14:26 +0200

    supp: change conn string

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,7 +1,9 @@
 ﻿using LiveChat.Models;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 namespace LiveChat
 {
     public class ApplicationDbContext : IdentityDbContext<User>
@@ -17,7 +19,7 @@ namespace LiveChat
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LIVECHAT;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Server = tcp:chatter - app.database.windows.net, 1433; Initial Catalog = LIVECHAT; Persist Security Info = False; User ID = CloudSA1e03a94c; Password = Jasa123!}; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
         }
         protected override void OnModelCreating(ModelBuilder builder)
         {
